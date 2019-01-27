fa071ce3d2f44aeeeba5a15f6ebecbe5452d4733
Innocent Angwadia <demsdema217@gmail.com>
2019-01-27 11:41:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Control F5 to run
+            Console.WriteLine("Hello World");
+        }
+    }
+}
